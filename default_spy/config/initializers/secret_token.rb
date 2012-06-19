ccc8ccaa3647b93a5932ebcca8b8f4a4fb1039ee
Diana Poudel <diana.poudel@gmail.com>
2012-06-19 18:56:05 +0300

    Initial commit

---
+++ default_spy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DefaultSpy::Application.config.secret_token = '02d1efee1877a446e8febc9c2029fb220fef1d98a200176a9eb54ca9252637f3a28bb855fafdd8f0fe6bf4546cd3d2ead4d0298baa9de15c309c5910657d38fc'
